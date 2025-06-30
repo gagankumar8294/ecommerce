851f79949bfa20c29e750f4a05ade5e6e9706fcb
gagankumar8294 <gagankumar8294@gmail.com>
2025-06-30 09:31:24 +0530

    redirecting from blog to certain blog page problem solved

--- comps/Blogs/data/blogsData.js
+++ comps/Blogs/data/blogsData.js
@@ -8,7 +8,7 @@ const blogsData = [
         { type: "title", value: "Top 5 Trending Bewakoof T-Shirts Under ₹599 in 2025" },
         { type: "image", value: "/blog_images/mens/blog1-10/bog1-images/featuredimage/bewakoof-graphic-tshirts-under-599-2025.jpg", alt: "Blog 1 Image" },
         { type: "paragraph", value: "stylish graphic tees that go well with jeans." },
-        { type: "url", value: "https://elitepurchase.vercel.app/blog/bewakoof-best-graphic-tshirts-under-599-2025"}
+        { type: "url", value: "https://elitepurchase.in/blog/bewakoof-best-graphic-tshirts-under-599-2025"}
       ],
     },
     
