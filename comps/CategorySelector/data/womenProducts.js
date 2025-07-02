3234aca8a761fca331c07d413f5425ee9c09a656
gagankumar8294 <gagankumar8294@gmail.com>
2025-07-02 13:08:16 +0530

    added women products in homeage

--- comps/CategorySelector/data/womenProducts.js
+++ comps/CategorySelector/data/womenProducts.js
@@ -1,21 +1,33 @@
 const womenProducts = [
     {
+      id: 1,
+      title: "Straight Green white color Kurti – Organza with Crepe Lining",
+      price: "₹499",
+      image: "/blog_images/women/blog1-10/kurthi-blog1-images/first/kurti-parrot-green-under-500-2025.jpg",
+    },
+    {
+      id: 2,
+      title: "Floral Chanderi Kurta with Elbow Sleeves – Straight Fit",
+      price: "₹499",
+      image: "/blog_images/women/blog1-10/kurthi-blog1-images/second/long-kurti-plus-size-with-pink-bottom.jpg",
+    },
+    {
+      id: 3,
+      title: "Chanderi Kurti for Women – Fenta Color (Sun Fashion)",
+      price: "₹474",
+      image: "/blog_images/women/blog1-10/kurthi-blog1-images/third/affordable-slim-fit-kurti-fashion-under-500-2025.jpg",
+    },
+    {
       id: 4,
-      title: "Women's Floral Dress",
-      price: "₹799",
-      image: "/images/women-dress.jpg",
+      title: "Pink Chanderi Blend Floral Kurti – Calf Length | Puff Sleeves",
+      price: "₹499",
+      image: "/blog_images/women/blog1-10/kurthi-blog1-images/fourth/office-wear-kurti-under-500.jpg",
     },
     {
       id: 5,
-      title: "Women's Kurti Set",
-      price: "₹899",
-      image: "/images/women-kurti.jpg",
-    },
-    {
-      id: 6,
-      title: "Women's Stylish Top",
-      price: "₹499",
-      image: "/images/women-top.jpg",
+      title: "Pink Rayon Blend Kurta Set with Dupatta – Embroidered | 3-Piece Suit",
+      price: "₹652",
+      image: "/blog_images/women/blog1-10/kurthi-blog1-images/sixth/embroidered-pink-kurta-set.png",
     },
   ];
   
