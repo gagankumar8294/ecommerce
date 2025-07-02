be7b28a75e5f2b74ad4ae204429841bdebeee082
gagankumar8294 <gagankumar8294@gmail.com>
2025-07-02 13:12:41 +0530

    added images + links to women products in homepage

--- comps/CategorySelector/data/womenProducts.js
+++ comps/CategorySelector/data/womenProducts.js
@@ -3,31 +3,45 @@ const womenProducts = [
       id: 1,
       title: "Straight Green white color Kurti – Organza with Crepe Lining",
       price: "₹499",
-      image: "/blog_images/women/blog1-10/kurthi-blog1-images/first/kurti-parrot-green-under-500-2025.jpg",
+      images: ["/blog_images/women/blog1-10/kurthi-blog1-images/first/kurti-parrot-green-under-500-2025.jpg"        
+      ],
+      link:''
     },
     {
       id: 2,
       title: "Floral Chanderi Kurta with Elbow Sleeves – Straight Fit",
       price: "₹499",
-      image: "/blog_images/women/blog1-10/kurthi-blog1-images/second/long-kurti-plus-size-with-pink-bottom.jpg",
+      images: [
+        "/blog_images/women/blog1-10/kurthi-blog1-images/second/long-kurti-plus-size-with-pink-bottom.jpg",
+      ],
+      link: ''
     },
     {
       id: 3,
       title: "Chanderi Kurti for Women – Fenta Color (Sun Fashion)",
       price: "₹474",
-      image: "/blog_images/women/blog1-10/kurthi-blog1-images/third/affordable-slim-fit-kurti-fashion-under-500-2025.jpg",
+      images: [
+        "/blog_images/women/blog1-10/kurthi-blog1-images/third/affordable-slim-fit-kurti-fashion-under-500-2025.jpg",
+      ],
+      link: 'https://amzn.to/3T4niWs'
     },
     {
       id: 4,
       title: "Pink Chanderi Blend Floral Kurti – Calf Length | Puff Sleeves",
       price: "₹499",
-      image: "/blog_images/women/blog1-10/kurthi-blog1-images/fourth/office-wear-kurti-under-500.jpg",
+      images: [
+        "/blog_images/women/blog1-10/kurthi-blog1-images/fourth/office-wear-kurti-under-500.jpg",
+      ],
+      link: ""
     },
     {
       id: 5,
       title: "Pink Rayon Blend Kurta Set with Dupatta – Embroidered | 3-Piece Suit",
       price: "₹652",
-      image: "/blog_images/women/blog1-10/kurthi-blog1-images/sixth/embroidered-pink-kurta-set.png",
+      images: [
+        "/blog_images/women/blog1-10/kurthi-blog1-images/sixth/embroidered-pink-kurta-set.png",
+      ],
+      link: ""
     },
   ];
   
